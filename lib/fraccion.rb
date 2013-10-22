e30ed3160c362e77a756cc4ba277bc61c6df858c
Albano Yanes <ajyanreyu@gmail.com>
2013-10-22 09:23:05 +0100

    Añadido el fichero para definicion de la clase

---
+++ lib/fraccion.rb
@@ -0,0 +1,27 @@
+=begin
+**********************************************************
+* Autores: 
+       Teno González Dos Santos
+       Albano José Yanes Reyes
+* Fecha: 18/10/2013
+* Asignatura: Lenguejes y Paradigmas de Programación
+* Tercero de Grado en Ingeniería Informática
+* Universidad de la Laguna
+*
+* Practica 06: Desarrollo de una clase fraccion utilizando 
+*  el desarrollo dirigido por pruebas 
+***********************************************************
+=end
+
+#! /usr/bin/ruby
+
+class Fraccion
+	attr_reader :a, :b #a: numerador, b: denominador
+	def initialize(a, b)
+		#comprobamos que los datos de entradas sean numericos
+		raise unless a.is_a?(Numeric)
+		raise unless b.is_a?(Numeric)
+
+	end
+
+end
